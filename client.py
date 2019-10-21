e080478de142bb5ab226e99b71355f71a47e0638
karthik <karthik.bhat@miqdigital.com>
2019-10-22 00:59:45 +0530

    something workin'

--- client.py
+++ client.py
@@ -1,24 +1,30 @@
 import socket
-
+import sys
+import select
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
+    host = socket.gethostname()
+    port = 5000
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
 
-    message = input(" -> ")  # take input
+    con_list = [sys.stdin, client_socket]
 
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+    while 1:
+        read_sock, write_sock, err_sock = select.select(con_list, [], [])
 
-        print('Received from server: ' + data)  # show in terminal
+        for sock in read_sock:
+            if sock == client_socket:
+                data = client_socket.recv(1024).decode()
 
-        message = input(" -> ")  # again take input
+                print('Received from server: ' + data)  # show in terminal
 
-    client_socket.close()  # close the connection
+            else:
+                message = input(" -> ")
+
+                client_socket.send(message.encode())
+
 
 
 if __name__ == '__main__':
